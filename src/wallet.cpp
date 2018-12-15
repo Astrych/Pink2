c614ba20def7be2f9c9e5e18d6930127ad4becd0
Astrych <azzzirel@gmail.com>
2018-12-15 21:32:12 +0100

    Fix error

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -779,7 +779,7 @@ void CWalletTx::GetAmounts(list<pair<CTxDestination, int64_t> >& listReceived,
     {
         cout << "                   ====== CWalletTx::GetAmounts ===========" << endl;
         cout << "                   IsCoinBase or IsCoinStake" << endl;
-        cout << "                   pwallet->GetCredit(): " << pwallet->GetCredit() << endl;
+        cout << "                   pwallet->GetCredit(): " << CWallet::GetCredit(*this); << endl;
         cout << "                   ========================================" << endl;
     }
 
