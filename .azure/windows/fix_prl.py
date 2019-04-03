6dcb84a5b7e3e1c24d3e40424df479f896e97cc3
Astrych <azzzirel@gmail.com>
2019-04-03 07:46:29 +0200

    Fix fix fix

--- .azure/windows/fix_prl.py
+++ .azure/windows/fix_prl.py
@@ -12,8 +12,13 @@ for subpath in sys.argv[2:]:
     libs += glob.glob(subpath + '/**/libQt*.a', recursive=True) + \
         glob.glob(subpath + '/**/libq*.a', recursive=True)
 
+print("Libs:")
+print(libs)
+print("+"*50)
+
 for subpath in sys.argv[2:]:
     for filename in glob.glob(subpath + '/*.prl'):
+        print("For file: {}".format(filename))
         with open(filename, 'r') as file:
             file_content = file.read()
             new_content = re.sub(r'(?m)^QMAKE_PRL_BUILD_DIR.*\n?', '', file_content)
@@ -24,5 +29,8 @@ for subpath in sys.argv[2:]:
                     ' ' + sys.argv[1] + '/' + lib,
                     new_content,
                 )
+                print("="*60)
+                print(new_content)
+                print("="*60)
         with open(filename, 'w') as file:
             file.write(new_content)
